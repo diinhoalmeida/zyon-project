634d442d7a9bc8e2df2e0e1e5a23745a381b2360
Edilson Almeida <edifilho1409@outook.com>
2026-04-10 12:37:36 -0300

    ci: adicionando workflow de deploy para Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,8 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700&display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap' }
       ]
-    }
+    },
+    baseURL: '/zyon-project/' // Nome exato do seu repositório
   },
   vite: {
     optimizeDeps: {
